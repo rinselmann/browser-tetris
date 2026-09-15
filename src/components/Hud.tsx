ff849e40b64be711cbcac89ee1fc1349f52ab563
Ryan Inselmann <ryan@inselmann.org>
2026-09-15 10:30:07 -0500

    formatting fix

--- src/components/Hud.tsx
+++ src/components/Hud.tsx
@@ -8,12 +8,12 @@ export default function Hud() {
   return (
     <div className="pointer-events-none absolute inset-0 flex flex-col justify-between p-6 text-zinc-100">
       <header>
-        <h1 className="text-2xl font-semibold tracking-tight text-center">Tetris</h1>
+        <h1 className="text-center text-2xl font-semibold tracking-tight">Tetris</h1>
       </header>
       <div className="place-self-center" hidden={!isGameOver}>
         Game Over
       </div>
-      <dl className="flex gap-8 font-mono text-sm place-self-center">
+      <dl className="flex gap-8 place-self-center font-mono text-sm">
         <div>
           <dt className="text-zinc-400">Score</dt>
           <dd className="text-lg">{score}</dd>
