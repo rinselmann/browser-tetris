3e54eac40eac0dd28e314f311646d92e00059a21
Ryan Inselmann <ryan@inselmann.org>
2026-09-15 09:43:23 -0500

    feat: responsive gameplay area

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -3,6 +3,31 @@
 import { Canvas } from "@react-three/fiber";
 import { OrthographicCamera } from "@react-three/drei";
 import { Tetrion } from "./Tetrion";
+import { useThree } from "@react-three/fiber";
+import { useLayoutEffect } from "react";
+import { OrthographicCamera as ThreeOrthographicCamera } from "three";
+
+const BOARD_ROWS = 20;
+const BOARD_COLUMNS = 10;
+const BOARD_PADDING = 1.3;
+
+function ResponsiveBoardCamera() {
+  const { camera, size } = useThree();
+
+  useLayoutEffect(() => {
+    if (!(camera instanceof ThreeOrthographicCamera)) {
+      return;
+    }
+
+    camera.zoom = Math.min(
+      size.width / (BOARD_COLUMNS * BOARD_PADDING),
+      size.height / (BOARD_ROWS * BOARD_PADDING),
+    );
+    camera.updateProjectionMatrix();
+  }, [camera, size.height, size.width]);
+
+  return null;
+}
 
 /**
  * Scaffold scene. This exists to prove the WebGL pipeline works end to end;
@@ -10,13 +35,13 @@ import { Tetrion } from "./Tetrion";
  */
 export default function GameCanvas() {
   return (
-    <Canvas camera={{ position: [3, 3, 5], fov: 50 }} className="bg-board-bg">
+    <Canvas className="bg-board-bg">
       <OrthographicCamera
         makeDefault
         position={[0, 5, 0]}
         rotation={[-Math.PI / 2, 0, 0]}
-        zoom={40}
       />
+      <ResponsiveBoardCamera />
       <ambientLight intensity={0.6} />
       <directionalLight position={[5, 8, 5]} intensity={2} />
       <Tetrion />
