065570b3e41dd02b4910ebe00094b69104fe7025
Ryan Inselmann <ryan@inselmann.org>
2026-09-15 10:14:51 -0500

    fix lint error

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -4,40 +4,33 @@ import { Canvas } from "@react-three/fiber";
 import { OrthographicCamera } from "@react-three/drei";
 import { Tetrion } from "./Tetrion";
 import { useThree } from "@react-three/fiber";
-import { useLayoutEffect } from "react";
-import { OrthographicCamera as ThreeOrthographicCamera } from "three";
 
 const BOARD_ROWS = 20;
 const BOARD_COLUMNS = 10;
 const BOARD_PADDING = 1.3;
 
-function ResponsiveBoardCamera() {
-  const { camera, size } = useThree();
+export function GameCamera() {
+  const { size } = useThree();
 
-  useLayoutEffect(() => {
-    if (!(camera instanceof ThreeOrthographicCamera)) {
-      return;
-    }
+  const zoom = Math.min(
+    size.width / (BOARD_COLUMNS * BOARD_PADDING),
+    size.height / (BOARD_ROWS * BOARD_PADDING),
+  );
 
-    camera.zoom = Math.min(
-      size.width / (BOARD_COLUMNS * BOARD_PADDING),
-      size.height / (BOARD_ROWS * BOARD_PADDING),
-    );
-    camera.updateProjectionMatrix();
-  }, [camera, size.height, size.width]);
-
-  return null;
+  return (
+    <OrthographicCamera
+      makeDefault
+      position={[0, 5, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      zoom={zoom}
+    />
+  );
 }
 
 export default function GameCanvas() {
   return (
     <Canvas className="bg-board-bg">
-      <OrthographicCamera
-        makeDefault
-        position={[0, 5, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-      />
-      <ResponsiveBoardCamera />
+      <GameCamera />
       <ambientLight intensity={0.6} />
       <directionalLight position={[5, 8, 5]} intensity={2} />
       <Tetrion />
